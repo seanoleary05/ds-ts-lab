74dad5c84d368b5977412132d048a55a10a5211b
seanoleary05 <seanoleary2005@gmail.com>
2026-09-16 16:20:45 +0100

    TODO: Finish 05

---
+++ src/05-utilityTypes.ts
@@ -0,0 +1,11 @@
+import { friends, colleagues } from "./01-basics";
+import { Friend, Colleague, SecureFriendContact, FriendPartial } from "./myTypes";
+
+function updateFriend(friend: Friend, updates: FriendPartial ) : Friend {
+  return { ...friend, ...updates}
+}
+
+console.log(updateFriend(friends[0], {
+  phone: '08712345',
+  dob: new Date("1998-10-22")
+}))
